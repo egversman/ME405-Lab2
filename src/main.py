f84db4695c802687b5adbeabb3e70c5a6002e6bb
dave-reo <davidgiovannireo@gmail.com>
2023-02-02 13:18:21 -0800

    initial docs

---
+++ src/main.py
@@ -0,0 +1,12 @@
+"""! @file main.py
+   
+   Write main code which runs the controller, running closed-loop step response
+   tests in which the setpoint is changed so as to rotate the motor by about one
+   revolution and stop it at the final position. The controller should run about
+   every 10ms or so; you can use function utime.sleep_ms() to roughly control the
+   controller’s rate of execution. The code which creates the controller object and c
+   auses it to run periodically should be in main.py.
+
+
+  
+"""
