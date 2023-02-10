2992ab9e4e5e22994c2d42d765f5ac3e678d9b31
dave-reo <davidgiovannireo@gmail.com>
2023-02-09 20:28:49 -0800

    it works

--- src/serial_test.py
+++ src/serial_test.py
@@ -1,5 +1,4 @@
 import serial
-import csv
 from matplotlib import pyplot as plt
 
 split = lambda line: line.strip().split(b',')[:2]
@@ -16,40 +15,43 @@ def process_data():
     data = []
     with serial.Serial('COM4', 115200, timeout=10) as ser:
 
-        for line in ser:
-            try:
-                data = line.readline().strip().split(b',')
-                to_float(data[0])
-                to_float(data[1])
+        while "False":
+            line = ser.readline() #.strip()
+            print(line)
 
-            except ValueError:
-                continue  # go to the next iteration of the loop
+            if b"done" in line.lower():
+                break
+
+            data = split(line)
+
+            if False in {data[0], data[1]}:
+                print(b"problem, idiot!")
             else:
                 x.append(float(data[0]))
                 y.append(float(data[1]))
+
     return x, y
 
 def generate_plot(x: list, y: list):
-   # plt.plot(x, y)
-    plt.xlabel('Time [sec? msec?]')
+    plt.plot(x, y)
+    plt.xlabel('Time [msec]')
     plt.ylabel('Motor Position [Encoder Ticks?]')
     plt.scatter(x, y)
     plt.show()
-    
-    
+
 if __name__ == "__main__":
     setpoint = str(input('Enter desired setpoint: '))
     Kp = str(input('Enter desired Kp: '))
     b_setpoint = setpoint.encode()
     b_Kp = Kp.encode()
 
-    with serial.Serial('COM4', 115200, timeout = 10) as ser:
+    with serial.Serial('COM4', 115200, timeout = 1000) as ser:
         ser.write(b_setpoint + b"\r\n")
         ser.write(b_Kp + b"\r\n")
 
-    print('Controller parameters sent')
-
-generate_plot(*process_data())
+        # print('Controller parameters sent')
+    x, y = process_data()
+    generate_plot(x, y)
 
 # runs step response tests
     # send characters through USB serial port to Micropython board
@@ -66,4 +68,4 @@ generate_plot(*process_data())
 
 
 
-                
+
