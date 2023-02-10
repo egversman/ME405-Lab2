3c6489301f99a42287917f1b6c2e3269032e2d84
dave-reo <davidgiovannireo@gmail.com>
2023-02-09 20:28:02 -0800

    it works

--- src/main.py
+++ src/main.py
@@ -1,48 +1,51 @@
 import motor_driver
 import encoder_reader
 import clp_controller
-# import serial_test
 import utime
 import pyb
 
-
-u2 = pyb.UART(2, baudrate=115200, timeout = 10)
-#u2.write when we want to write
+u2 = pyb.UART(2, baudrate=115200, timeout = 10000)
  
 motor_dvr = motor_driver.MotorDriver() # creates a working motor
 encoder = encoder_reader.EncoderReader() # creates a working encoder
 controller = clp_controller.CLPController() # creates a working controller
-#continue_char = 'y'
-
-
 
 u2.write(str(controller.setpoint).encode())
 
+#while (not u2.any()):
+#    pyb.delay(10)
+#u2.write(b'setpoint please\r\n')
 
+setpoint = u2.readline() #.strip()
+#u2.write(b"enter kp\r\n")
 
-while (not u2.any()):
-    pyb.delay(10)
-
-setpoint = int(u2.readline().strip())
 kp = float(u2.readline())
-
+#u2.write(b"ok\r\n")
 controller.set_setpoint(setpoint)
 controller.set_Kp(kp)
 
 
 encoder.zero()
-start_time = utime.ticks_ms() 
-    
-for _ in range(5000):
+start_time = utime.ticks_ms()
+
+
+for i in range(500):
     meas_pos = encoder.read()
-    motor_dvr.set_duty_cycle(controller.run(controller.setpoint, meas_pos))
-    
+    #u2.write("setpoint type: " + str(type(controller.setpoint)) + ", pos type: " + str(type(meas_pos)))
+    motor_dvr.set_duty_cycle(controller.run(int(controller.setpoint.decode()), meas_pos))
     controller.times.append(utime.ticks_ms() - start_time)
     controller.motor_positions.append(meas_pos)
+    u2.write(str(controller.times[i]).encode() + b", " + str(controller.motor_positions[i]).encode() + b"\r\n")
+ 
+    utime.sleep_ms(50)
     
-    utime.sleep_ms(10000)
-    
-u2.write(controller.print_response().encode('utf-8')) #something up over here
+u2.write(b"Done!\r\n")    
+motor_dvr.set_duty_cycle(0)
+#u2 = pyb.UART(2, baudrate=115200, timeout = 10)
+
+#for i in range(len(controller.motor_positions)):
+#    u2.write(f'{controller.times[i]}, {control.motor_positions[i]}\r\n')
+ #something up over here
     
     
 
